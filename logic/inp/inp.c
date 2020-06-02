fb8b8d9444657588c934b671c911717f7e5b7142
AVCADO <devavcado@gmail.com>
2020-06-02 19:04:31 +0000

    Added input function, will work on interpreter

---
+++ logic/inp/inp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char inp(char ask[]){
+    char ret[100];
+    printf("%s", ask);
+    scanf("%s", &ret);
+}
+
+
+int main(){
+    inp("This is a test ");
+}

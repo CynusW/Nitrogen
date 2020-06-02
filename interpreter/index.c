0e8d2092dae572b4b83efe71a05daee12e514f31
AVCADO <devavcado@gmail.com>
2020-06-02 19:59:25 +0000

    Interpreter prototype

---
+++ interpreter/index.c
@@ -0,0 +1,11 @@
+// The base of the interpreter
+
+
+// imports
+#include <stdio.h>
+#include <stdlib.h>
+#include "interpret.c"
+
+int main(){
+    UnixShell();
+}
---
+++ interpreter/interpret.c
@@ -0,0 +1,49 @@
+// Interpret.c interprets all the commands
+// and also runs the shell.
+
+
+// Imports
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// functions
+void run(char file[]){
+    // TODO: add
+}
+
+void UnixShell(){
+    // an infinite loop
+    char command[500];
+    for(; ;){
+        printf("N> ");
+        scanf("%s", &command);
+        if(strstr(command, "log")){
+            for(int i = 0; i < strlen(command) - 5; i++){
+                if(strstr(command, "\")")) break;
+                // printf();
+                system("./build/unix/log.out");
+                break;
+            }
+        } else {
+            printf("Syntax error - interpreter.nit(1:1)\n");
+            printf("An error occured and...yeah the thing crashed\n");
+            printf("Details of error: %s\n", command);
+            break;
+        }
+        if (strstr(command, "inp")){
+            for(int i = 0; i < strlen(command) - 5; i++){
+                if (strstr(command, "\")")) break;
+                system("./build/unix/inp.out");
+                break;
+            }
+            break;
+        } else {
+            printf("Syntax error - interpreter.nit(1:1)\n");
+            printf("An error occured and...yeah the thing crashed\n");
+            printf("Details of error: %s\n", command);
+            break;
+        }
+    }
+}
